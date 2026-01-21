272c767fb78ba8debab15b7fc03b81995113c6af
dp1rk0v <gekepyrkov@gmail.com>
2026-01-21 04:40:06 +0300

    feat(theme): Add theme switching

---
+++ components/shared/theme-toggle.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      <Sun className="h-full w-full scale-100 dark:scale-0" />
+      <Moon className="absolute h-full w-full scale-0 dark:scale-100" />
+    </Button>
+  );
+}
